f314f2a5afb972d1117bd488af7ec3a74329a73a
Tomasz Przyborowski <t.s.przyborowski@gmail.com>
2020-05-09 15:45:11 +0200

    add gulpfile gitignore create new fodlers to work

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const playerName = {
-    name: "",
+  name: "",
 };
 
 const gameSummary = {
-    games: 0,
-    wins: 0,
-    loses: 0,
-    draws: 0,
+  games: 0,
+  wins: 0,
+  loses: 0,
+  draws: 0,
 };
 
 const game = {
-    playerHand: "",
-    compHand: "",
+  playerHand: "",
+  compHand: "",
 };
 
 const hands = [...document.querySelectorAll(".select-container img")];
@@ -22,106 +22,106 @@ const playBtn = document.querySelector(".play-btn")
 
 // Submit your name function
 const playerNameCreator = (e) => {
-    e.preventDefault();
-    playerName.name = input.value;
-    console.log(playerName);
-    input.value = "";
+  e.preventDefault();
+  playerName.name = input.value;
+  console.log(playerName);
+  input.value = "";
 };
 
 nameBtn.addEventListener("click", playerNameCreator)
 
 // Display chosen name in game introduction function
 const showIntroduction = () => {
-    playerIntr.textContent = `Choose your destiny ${playerName.name}-san...`;
+  playerIntr.textContent = `Choose your destiny ${playerName.name}-san...`;
 };
 
 nameBtn.addEventListener("click", showIntroduction)
 
 // Select your hand function
 const handSelector = (e) => {
-    game.playerHand = e.target.dataset.option
-    console.log(game.playerHand);
-    for (const hand of hands) {
-        hand.style.boxShadow = "";
-    };
-    e.target.style.boxShadow = "0 0 0 4px yellow";
+  game.playerHand = e.target.dataset.option
+  console.log(game.playerHand);
+  for (const hand of hands) {
+    hand.style.boxShadow = "";
+  };
+  e.target.style.boxShadow = "0 0 0 4px yellow";
 };
 
 for (const hand of hands) {
-    hand.addEventListener("click", handSelector);
+  hand.addEventListener("click", handSelector);
 };
 
 //Random hand function
 const randomHand = () => {
-    let randomIndex = Math.floor(Math.random() * 3);
-    let randomHand = hands[randomIndex].dataset.option;
-    return randomHand;
+  let randomIndex = Math.floor(Math.random() * 3);
+  let randomHand = hands[randomIndex].dataset.option;
+  return randomHand;
 };
 
 //Check results function
 const checkResult = (player, comp) => {
-    if (player === comp) {
-        ++gameSummary.games;
-        ++gameSummary.draws;
-        return "draw";
-    } else if ((player === "paper" && comp === "rock") || (player === "rock" && comp === "scissors") || (player === "scissors" && comp === "paper")) {
-        ++gameSummary.games;
-        ++gameSummary.wins;
-        return "win";
-    } else {
-        ++gameSummary.games;
-        ++gameSummary.loses;
-        return "lose";
-    };
+  if (player === comp) {
+    ++gameSummary.games;
+    ++gameSummary.draws;
+    return "draw";
+  } else if ((player === "paper" && comp === "rock") || (player === "rock" && comp === "scissors") || (player === "scissors" && comp === "paper")) {
+    ++gameSummary.games;
+    ++gameSummary.wins;
+    return "win";
+  } else {
+    ++gameSummary.games;
+    ++gameSummary.loses;
+    return "lose";
+  };
 };
 
 //Show results in HTML function
 const showResults = (player, comp, result) => {
-    const playerChoice = document.querySelector('[data-summary="your-choice"]');
-    playerChoice.textContent = player;
-    const compChoice = document.querySelector('[data-summary="cp-choice"]');
-    compChoice.textContent = comp;
-    const gameNumbers = document.querySelector("p.games span");
-    gameNumbers.textContent = gameSummary.games;
-    const winNumbers = document.querySelector("p.wins span");
-    winNumbers.textContent = gameSummary.wins;
-    const drawNumbers = document.querySelector("p.draws span");
-    drawNumbers.textContent = gameSummary.draws;
-    const loseNumbers = document.querySelector("p.loses span");
-    loseNumbers.textContent = gameSummary.loses;
+  const playerChoice = document.querySelector('[data-summary="your-choice"]');
+  playerChoice.textContent = player;
+  const compChoice = document.querySelector('[data-summary="cp-choice"]');
+  compChoice.textContent = comp;
+  const gameNumbers = document.querySelector("p.games span");
+  gameNumbers.textContent = gameSummary.games;
+  const winNumbers = document.querySelector("p.wins span");
+  winNumbers.textContent = gameSummary.wins;
+  const drawNumbers = document.querySelector("p.draws span");
+  drawNumbers.textContent = gameSummary.draws;
+  const loseNumbers = document.querySelector("p.loses span");
+  loseNumbers.textContent = gameSummary.loses;
 
-    if (result === "win") {
-        const winner = document.querySelector('[data-summary="who-win"]');
-        winner.textContent = `${playerName.name}-san`;
-    } else if (result === "lose") {
-        const winner = document.querySelector('[data-summary="who-win"]');
-        winner.textContent = "Computer-san";
-    } else {
-        const winner = document.querySelector('[data-summary="who-win"]');
-        winner.textContent = "nobody";
-    };
+  if (result === "win") {
+    const winner = document.querySelector('[data-summary="who-win"]');
+    winner.textContent = `${playerName.name}-san`;
+  } else if (result === "lose") {
+    const winner = document.querySelector('[data-summary="who-win"]');
+    winner.textContent = "Computer-san";
+  } else {
+    const winner = document.querySelector('[data-summary="who-win"]');
+    winner.textContent = "nobody";
+  };
 
 };
 // Ending game function
 const endGame = () => {
-    document.querySelector(`[data-option="${game.playerHand}"]`).style.boxShadow = "";
-    game.playerHand = "";
-    game.compHand = "";
+  document.querySelector(`[data-option="${game.playerHand}"]`).style.boxShadow = "";
+  game.playerHand = "";
+  game.compHand = "";
 };
 
 
 // Starting game function
 const startGame = () => {
-    if (game.playerHand) {
-        game.compHand = randomHand();
-        console.log(game.compHand);
-        const gameResult = checkResult(game.playerHand, game.compHand);
-        console.log(gameResult);
-        showResults(game.playerHand, game.compHand, gameResult);
-        endGame();
-    } else {
-        return alert("Choose your hand!");
-    };
+  if (game.playerHand) {
+    game.compHand = randomHand();
+    console.log(game.compHand);
+    const gameResult = checkResult(game.playerHand, game.compHand);
+    console.log(gameResult);
+    showResults(game.playerHand, game.compHand, gameResult);
+    endGame();
+  } else {
+    return alert("Choose your hand!");
+  };
 };
 
 playBtn.addEventListener("click", startGame);
